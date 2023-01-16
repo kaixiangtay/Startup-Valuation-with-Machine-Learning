60ff8eb70394e51807113957985f2c5379108d67
Tay Kai Xiang <59237953+kaixiangtay@users.noreply.github.com>
2023-01-16 20:45:52 +0800

    Add files via upload
    Add AWS Lambda function file

---
+++ scripts/lambda_function.py
@@ -0,0 +1,29 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime = boto3.client('runtime.sagemaker')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    data = json.loads(json.dumps(event))
+
+    payload = data['startup']
+
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='text/csv',
+                                       Body=payload)
+
+    result = json.loads(response['Body'].read().decode())
+
+    startup_valuation_prediction = 10 ** result
+    startup_valuation_prediction_label = f"Startup Predicted Valuation Amount: ${startup_valuation_prediction}"
+
+    print("Startup Predicted Valuation Amount: ", startup_valuation_prediction)
+
+    return startup_valuation_prediction_label
