6229b3d09e31cb72d6c781b8eca22d551dbf8461
simran-bhadani3 <simranbhadani333@gmail.com>
2023-01-25 15:45:07 +0800

    some ui changes

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -58,7 +58,7 @@ class Form extends Component {
         event.preventDefault();
 
         axios
-            .post("https://nmzgzgb7gl.execute-api.ap-southeast-1.amazonaws.com/api/startupvaluationprediction", {
+            .post("https://qw16rmz5wa.execute-api.ap-southeast-1.amazonaws.com/api/startupvaluationprediction", {
                 data: `${this.state.Unnamed}, ${this.state.funding_rounds},
             ${this.state.funding_total_usd},
             ${this.state.number_of_members},
@@ -96,6 +96,47 @@ class Form extends Component {
                     ${this.state.day}`})
             .then((res) => {
                 this.setState({
+                    "Unnamed": "0",
+                    "funding_rounds": "",
+                    "funding_total_usd": "",
+                    "number_of_members": "",
+                    "number_of_founders": "",
+                    "mean_funding": "",
+                    "max_funding": "",
+                    "crowdfunding": "",
+                    "post_ipo": "",
+                    "private_equity": "",
+                    "venture": "",
+                    "a": "",
+                    "angel": "",
+                    "b": "",
+                    "c": "",
+                    "convertible": "",
+                    "crowd": "",
+                    "crowd_equity": "",
+                    "d": "",
+                    "debt_round": "",
+                    "e": "",
+                    "f": "",
+                    "g": "",
+                    "grant": "",
+                    "partial": "",
+                    "post_ipo_debt": "",
+                    "post_ipo_equity": "",
+                    "private_equity_two": "0",
+                    "secondary_market": "",
+                    "seed": "",
+                    "unattributed": "",
+                    "number_of_invested_VCs": "",
+                    "total_investment_from_VCs": "",
+                    "year": "",
+                    "month": "",
+                    "day": "", });
+                console.log(res);
+                this.props.setValuation(res.body.split("$")[1]);
+            })
+            .catch((err) => {
+                this.setState({
                     "Unnamed": "",
                     "funding_rounds": "",
                     "funding_total_usd": "",
@@ -132,9 +173,7 @@ class Form extends Component {
                     "year": "",
                     "month": "",
                     "day": "", });
-                this.props.setValuation(res.data);
-            })
-            .catch((err) => {
+                console.log(err);
                 this.setState({ errors: err });
             });
     }
@@ -144,62 +183,32 @@ class Form extends Component {
             <div className="form">
                 <h2>Get Your Prediction!</h2>
                 <form onSubmit={this.onSubmit}>
-                    <div >Unnamed: </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="Unnamed"
-                        name="Unnamed"
-                        value={this.state.Unnamed}
-                        onChange={this.onChange}
-                    ></input>
-                    <div>Number of funding rounds: </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="funding_rounds"
-                        name="funding_rounds"
-                        value={this.state.funding_rounds}
-                        onChange={this.onChange}
-                    ></input>
-                    <div>Total funding (USD): </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="funding_total_usd"
-                        name="funding_total_usd"
-                        value={this.state.funding_total_usd}
-                        onChange={this.onChange}
-                    ></input>
-                    <div>Mean funding amount (USD): </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="mean_funding"
-                        name="mean_funding"
-                        value={this.state.mean_funding}
-                        onChange={this.onChange}
-                    ></input>
-                    <div>Maximum funding amount (USD): </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="max_funding"
-                        name="max_funding"
-                        value={this.state.max_funding}
-                        onChange={this.onChange}
-                    ></input>
-                    <div>Maximum funding amount (USD): </div>
-                    <input
-                        className=""
-                        type="text"
-                        htmlFor="max_funding"
-                        name="max_funding"
-                        value={this.state.max_funding}
-                        onChange={this.onChange}
-                    ></input>
+                    <div className="input-field">
+                        <label className="required">Unnamed: </label>
+                        <input
+                            className="input"
+                            type="number"
+                            name="Unnamed"
+                            value={this.state.Unnamed}
+                            onChange={this.onChange}
+                            min={0}
+                            required
+                        ></input>
+                    </div>
+                    <div className="input-field">
+                        <label>Number of funding rounds:</label>
+                        <input
+                            className="input"
+                            type="number"
+                            name="funding_rounds"
+                            value={this.state.funding_rounds}
+                            onChange={this.onChange}
+                            min={0}
+                            step={1}
+                        ></input>
+                    </div>
                     <div>
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Submit" className="submit-btn"/>
                     </div>
                 </form>
             </div>
